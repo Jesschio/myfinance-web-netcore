2b9a50a7308b14248f5b511731a5616767d50b7a
Jesschio <111002681+Jesschio@users.noreply.github.com>
2022-08-29 13:14:45 -0300

    Update Transacao.cs

--- myfinance-web-netcore/Domain/Transacao.cs
+++ myfinance-web-netcore/Domain/Transacao.cs
@@ -2,79 +2,89 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using myfinance_web_netcore.infra;
 using myfinance_web_netcore.Models;
+
+using myfinance_web_netcore.Infra;
 
 namespace myfinance_web_netcore.Domain
 {
     public class Transacao
     {
-        private const int V = 0;
 
+            public void Deletar(int id)
+        {
+            var objDAL = DAL.GetInstancia;
+            objDAL.Conectar();
+            string sql = $"DELETE FROM TRANSACAO WHERE ID = {id}";
+            objDAL.ExecutarComandoSQL(sql);
+            objDAL.Desconectar();
+        }
+
+            public void Atualizar (TransacaoModel formulario)
+        {
+            var objDAL  = DAL.GetInstancia;
+            objDAL.Conectar();
+            var sql = $"UPDATE TRANSACAO SET DATA = '{formulario.Data.ToString("yyyyMMdd")}'," +
+                $"VALOR = {formulario.Valor}, TIPO = '{formulario.Tipo}', HISTORICO = '{formulario.Historico}', " +
+                $"ID_PLANO_CONTA = {formulario.IdPlanoConta} WHERE ID = {formulario.Id}";
+
+            objDAL.ExecutarComandoSQL(sql);
+            objDAL.Desconectar();
+        }
         public void Inserir(TransacaoModel formulario)
         {
             var objDAL = DAL.GetInstancia;
             objDAL.Conectar();
 
-            var sql = "INSERT INTO TRANSACAO (data,valor,tipo, historico,id_plano_conta) " +
+            var sql = "INSERT INTO TRANSACAO(data, valor, tipo, historico, id_plano_conta) " +
             "VALUES(" +
-            $"'{formulario.Data.ToString("yyyy-MM-dd")}'," +
-            $"{formulario.Valor}," +
-            $"'{formulario.Tipo}'," +
-            $"'{formulario.Historico}'," +
+            $"'{formulario.Data.ToString("yyyyMMdd")}',"+
+            $"{formulario.Valor},"+
+            $"'{formulario.Tipo}',"+            
+            $"'{formulario.Historico}',"+
             $"{formulario.IdPlanoConta})";
+            //$"{formulario.Pagamento}";
+
+            Console.WriteLine("Comando SQL: " + sql.ToString());
 
             objDAL.ExecutarComandoSQL(sql);
             objDAL.Desconectar();
         }
-         public void Atualizar(TransacaoModel formulario)
-        {
-            var objDAL = DAL.GetInstancia;
-            objDAL.Conectar();
-            var sql = $"UPDATE TRANSACAO SET " +
-            $"Historico = '{formulario.Historico}'," +
-            $"Tipo = '{formulario.Tipo}', " +
-            $"Valor = {formulario.Valor} ," +
-            $"Data = '{formulario.Data.ToString("yyyy-MM-dd")}', " +
-            $"id_plano_conta = '{formulario.IdPlanoConta}' " +
-            $"WHERE id = {formulario.Id}";
 
-            objDAL.ExecutarComandoSQL(sql);
-            objDAL.Desconectar();
-        }
-        public TransacaoModel CarregarTransacaoPorId (int? id)
+            public TransacaoModel CarregarTransacaoPorId(int? id)
         {
-         var objDAL = DAL.GetInstancia;
+            var objDAL  = DAL.GetInstancia;
             objDAL.Conectar();
 
-            var sql = $"select id, data, valor, tipo, historico, id_plano_conta from transacao WHERE ID = {id}";
+            var sql = $"SELECT ID, DATA, VALOR,TIPO, HISTORICO, ID_PLANO_CONTA FROM TRANSACAO where id = {id}";
             var dataTable = objDAL.RetornarDataTable(sql);
-            
-            var transacao = new TransacaoModel()
-            {
-                Id = int.Parse(dataTable.Rows[0]["ID"].ToString()),
-                Historico = dataTable.Rows[0]["HISTORICO"].ToString(),
-                Tipo = dataTable.Rows[0]["TIPO"].ToString(),
-                Data = DateTime.Parse(dataTable.Rows[0]["data"].ToString()),
-                Valor = decimal.Parse(dataTable.Rows[0]["VALOR"].ToString()),
-                IdPlanoConta = int.Parse(dataTable.Rows[0]["ID"].ToString()),
-            };
-             objDAL.Desconectar();
+
+                
+    		var transacao = new TransacaoModel(){
+                    Id = int.Parse (dataTable.Rows[0]["ID"].ToString()),
+                    Historico = dataTable.Rows[0]["HISTORICO"].ToString(),
+                    Tipo = dataTable.Rows[0]["TIPO"].ToString(),
+                    Data = DateTime.Parse(dataTable.Rows[0]["DATA"].ToString()),
+                    Valor = decimal.Parse(dataTable.Rows[0]["VALOR"].ToString()),  
+                    IdPlanoConta = int.Parse(dataTable.Rows[0]["ID_PLANO_CONTA"].ToString()),                                 
+                };
+
+            objDAL.Desconectar();
             return transacao;
         }
 
-         public List<TransacaoModel> ListaTransacoes()
+        public List<TransacaoModel> ListaTransacoes()
         {
             List<TransacaoModel> lista = new List<TransacaoModel>();
-
             var objDAL = DAL.GetInstancia;
             objDAL.Conectar();
 
-            var sql = "select id, data, valor, tipo, historico, id_plano_conta from transacao";
+            var sql = "SELECT ID, DATA, VALOR,TIPO, HISTORICO, ID_PLANO_CONTA FROM TRANSACAO";
             var dataTable = objDAL.RetornarDataTable(sql);
 
-             for (int i=0; i < dataTable.Rows.Count; i++)
-             {
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+
                 var transacao = new TransacaoModel()
                 {
                     Id = int.Parse(dataTable.Rows[i]["ID"].ToString()),
@@ -83,13 +93,14 @@ namespace myfinance_web_netcore.Domain
                     Tipo = dataTable.Rows[i]["TIPO"].ToString(),
                     Historico = dataTable.Rows[i]["HISTORICO"].ToString(),
                     IdPlanoConta = int.Parse(dataTable.Rows[i]["ID_PLANO_CONTA"].ToString()),
-                    
+                    //Pagamento = int.Parse(dataTable.Rows[i]["PAGAMENTO"].ToString())
                 };
+
                 lista.Add(transacao);
-             }
-             objDAL.Desconectar();
+            }
+            objDAL.Desconectar();
+
             return lista;
         }
-        
     }
 }
